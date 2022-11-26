440ab9aa17484ec8bd1f974e55fda50affec3eec
kevinpaul99 <kevinpaul99999@gmail.com>
2022-11-25 19:13:39 -0500

    Nueva carpeta

---
+++ deber.js
@@ -0,0 +1,25 @@
+console.log("Caracteristicas de un Nissan 350Z")
+const na="Nissan 350Z";
+const aa="2008";
+const vm="250 km/h";
+var c="Gasolina";
+const a0100="5.7 s";
+const model="350Z Coupé 3.5 V6 Pack";
+const price="34.655$";
+const pricefiva="45.318";
+console.log("Nombre del auto: ");
+console.log(na);
+console.log("Año del auto: ");
+console.log(aa);
+console.log("Velocidad maxima: ");
+console.log(vm);
+console.log("Combustible: ");
+console.log(c);
+console.log("Aceleracion del 0 al 100: ");
+console.log(a0100);
+console.log("Modelo del auto: ");
+console.log(model);
+console.log("Precio del auto:");
+console.log(price);
+console.log("Precio final del auto con IVA y matricula:");
+console.log(pricefiva);
