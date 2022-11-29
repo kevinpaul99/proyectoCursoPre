39cc733a622c9327df186d5861431e0cf09e400c
kevinpaul99 <kevinpaul99999@gmail.com>
2022-11-28 19:40:52 -0500

    jj

---
+++ main.js
@@ -0,0 +1,20 @@
+var auto={
+    na:"Nissan 350z",
+    aa:"2008",
+    vm:"250km",
+    c:"Gasolina",
+    a0100:"5.7s",
+};
+
+console.log(auto.aa);
+//Actividad generar 3 objetos
+var pc={
+    tm=Montherboard h510 Chip Intel ;
+    pr=Intel Core I7 11700 2.50 GHz 11va Generación;
+    tg=;
+    fdp=;
+    dd=;
+    mm=;
+    gn=;
+}
+console.log(auto.pr);
