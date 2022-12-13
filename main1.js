3bef91829ce11cca09af56b68e01407b345eab2b
kevinpaul99 <kevinpaul99999@gmail.com>
2022-12-12 19:54:43 -0500

    nuevo deber

---
+++ main1.js
@@ -0,0 +1,66 @@
+var pc={
+    tm:"Asus Prime H410M-A" ,
+    pr:"Core I7-10700 2.90GHZ UP 4.8 GHZ",
+    tg:"GTX-1660 6GB DDR6",
+    fdp:"Corsair CV 650W 80 Plus Bronce",
+    dd:"Ssd 480GB",
+    mr:"16GB DDR4",
+    gn:"Case Gamer K869",
+};
+console.log("Caracteristicas de una pc");
+console.log("Tarjeta madre:");
+console.log(pc.tm);
+console.log("Procesador:");
+console.log(pc.pr);
+console.log("Targeta grafica:");
+console.log(pc.tg);
+console.log("Fuente de poder:");
+console.log(pc.fdp);
+console.log("Disco duro:");
+console.log(pc.dd);
+console.log("Memoria RAM:");
+console.log(pc.mr);
+console.log("Cases:");
+console.log(pc.gn);
+var foco={
+    w:"10" ,
+    vac:"220",
+    lm:"910",
+    ct:"3000",
+    cri:"85",
+    p:"200",
+    ai:"270",
+};
+console.log("Especificaciones de una foco OBA-10LB-300K");
+console.log("Potencia de consumo(W):");
+console.log(foco.w);
+console.log("Entrada de voltaje (VAC):");
+console.log(foco.vac);
+console.log("Flujo luminoso (lm):");
+console.log(foco.lm);
+console.log("Color de temperatura (K):");
+console.log(foco.ct);
+console.log("CRI(Ra):");
+console.log(foco.cri);
+console.log("Peso(g):");
+console.log(foco.p);
+console.log("Angulo de iluminacion(Deg):");
+console.log(foco.ai);
+var bic={
+    t:"10" ,
+    com:"Sram Force 2x11 velocidades",
+    bielas:"48/32",
+    r:"Mavic Cosmic",
+    ll:"Maxxis Cormet",
+};
+console.log("Especificaciones de una bicicleta Specialized Transition carbono");
+console.log("Talla:");
+console.log(bic.t);
+console.log("Componentes:");
+console.log(bic.com);
+console.log("Bielas OVAL:");
+console.log(bic.bielas);
+console.log("Ruedas:");
+console.log(bic.r);
+console.log("Llantas:");
+console.log(bic.ll);
